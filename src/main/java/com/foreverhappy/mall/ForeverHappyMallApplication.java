a9cf03b02777e8fc82bedc6825abf73113482ce3
Yuchuan <liuyuchuan816@163.com>
2020-11-02 22:44:15 +0800

    init

---
+++ src/main/java/com/foreverhappy/mall/ForeverHappyMallApplication.java
@@ -0,0 +1,13 @@
+package com.foreverhappy.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeverHappyMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeverHappyMallApplication.class, args);
+    }
+
+}
