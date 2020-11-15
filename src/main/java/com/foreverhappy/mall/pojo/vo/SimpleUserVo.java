2e1924f9be3a4f901fe646ad5aa1c4eaf60360cd
Yuchuan <liuyuchuan816@163.com>
2020-11-15 19:47:07 +0800

    单元测试

---
+++ src/main/java/com/foreverhappy/mall/pojo/vo/SimpleUserVo.java
@@ -0,0 +1,11 @@
+package com.foreverhappy.mall.pojo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SimpleUserVo {
+    private String userName;
+
+}
---
+++ src/main/java/com/foreverhappy/mall/web/UserController.java
@@ -0,0 +1,15 @@
+package com.foreverhappy.mall.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/")
+    public Object getUserList() {
+        return "success";
+    }
+}
